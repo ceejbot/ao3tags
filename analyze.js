25aac71d5d57184f4e7c44b9a180443d83835687
C J Silverio <ceejceej@gmail.com>
2013-03-23 11:11:26 -0700

    Added a --filter option that lists matching tags.
    Matches ignore case.

--- analyze.js
+++ analyze.js
@@ -6,15 +6,34 @@ var
 		.alias('c', 'cutoff')
 		.default('c', 100)
 		.describe('c', 'the tag usage cutoff')
+		.alias('f', 'filter')
+		.default('f', undefined)
+		.describe('f', 'string or pattern to filter for')
+	util = require('util'),
+	args = optimist.argv
 	;
 
 var tags = JSON.parse(fs.readFileSync('tags.json', 'utf8'));
-
 var keys = Object.keys(tags);
-var popular = keys.filter(function(item)
-{
-	return (tags[item] > optimist.argv.c);
-});
+var interesting = [];
 
-console.log(popular);
-console.log(popular.length);
+if (args.f)
+{
+	var pattern = new RegExp(args.f, 'i');
+
+	interesting = keys.filter(function(item)
+	{
+		return (pattern.test(item));
+	});
+}
+else
+{
+	interesting = keys.filter(function(item)
+	{
+		return (tags[item] > args.c);
+	});
+
+}
+
+console.log(util.inspect(interesting, {colors: true}));
+console.log(interesting.length);
